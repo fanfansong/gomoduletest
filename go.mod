12b6f47d81ad13dcb8b761f3c65d2603c5904e68
宋慧琳 <fanfansong@songhuilindeMacBook-Pro.local>
2019-07-10 19:42:32 +0800

    v2

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/fanfansong/gomoduletest
+module github.com/fanfansong/gomoduletest/v2
 
 go 1.12
 
--- test1/test1.go
+++ test1/test1.go
@@ -2,17 +2,17 @@ package test1
 
 import "github.com/apex/log"
 
-func String() bool {
-	log.WithFields(log.Fields{
-		"package": "test1",
-	}).Info("info")
-	return true
-}
-
-// func String2() {
+// func String() bool {
 // 	log.WithFields(log.Fields{
-// 		"package":  "test1",
-// 		"function": "String2()",
+// 		"package": "test1",
 // 	}).Info("info")
 // 	return true
 // }
+
+func String2() {
+	log.WithFields(log.Fields{
+		"package":  "test1",
+		"function": "String2()",
+	}).Info("info")
+	return true
+}
